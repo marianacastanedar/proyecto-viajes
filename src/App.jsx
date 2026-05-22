7f56a79a75595c1775c063434fd48f8f295eada8
mar <cas24481@uvg.edu.gt>
2026-05-21 18:26:08 -0600

    estructura de formulario usando useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
+import { useState, useEffect } from "react";
+
+
 function App() {
-  return (
-    <div>
-      <h1>App de Viajes</h1>
-      <p>Fase 1</p>
-    </div>
-  );
+    /*
+    const [items, setItems] = useState(
+        () => JSON.parse(localStorage.getItem('items') || '[]')
+    );
+    useEffect(() => {
+        localStorage.setItem('items', JSON.stringify(items));
+    }, [items]);
+    */
+
+    //const [items, setItems] = useState(["hola", "Andres"]);
+    const [items, setItems] = useState(
+        () => JSON.parse(localStorage.getItem('items') || ["hola"])
+    );
+    useEffect(() => {
+        localStorage.setItem('items', JSON.stringify(items));
+    }, [items]);
+
+    const agregarItem = (nuevo) => {
+        setItems([...items, nuevo]);
+    }
+    const [texto, setTexto] = useState("");
+    
+    
+
+
+    return (
+        <div>
+            <p>hola</p>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                agregarItem(texto);
+                setTexto("");
+            }}>
+                <input
+                    value={texto}
+                    onChange={(e) => setTexto(e.target.value)}
+                />
+                <button type="submit">
+                    enviar
+                </button>
+            </form>
+
+            <button onClick={() => agregarItem("HOLA")}>
+                AGREGAR UN HOLA
+            </button>
+            {items.map(item =>
+                <p key={item}>{item}</p>
+            )}
+        </div>
+
+    );
+    
 }
 
 export default App;
